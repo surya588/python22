cdd1f9c80087b523d8735294184c791afb4b22b9
sambapython@gmail.com <sambapython@gmail.com>
2016-11-23 09:05:30 +0530

    2016-NOV-22

---
+++ memory organization.py
@@ -0,0 +1,174 @@
+
+# coding: utf-8
+
+# In[1]:
+
+a=10
+b=20
+c=a
+d=10
+print "fddsf"
+
+
+# In[2]:
+
+l1=[1,2,3,4]
+l2=[5,6,7,8]
+l3=l1
+l4=[1,2,3,4]
+
+
+# In[3]:
+
+print a
+
+
+# In[4]:
+
+print e
+
+
+# In[5]:
+
+print id(a)
+
+
+# In[6]:
+
+print c
+
+
+# In[9]:
+
+print id(c)
+print id(a)
+print id(d)
+
+
+# In[11]:
+
+print id(l1)
+print id(l3)
+print id(l4)
+
+
+# In[12]:
+
+a=100
+
+
+# In[13]:
+
+print a
+
+
+# In[14]:
+
+print c
+
+
+# In[15]:
+
+print d
+
+
+# In[16]:
+
+print id(d)
+
+
+# In[17]:
+
+d=300
+
+
+# In[18]:
+
+print id(d)
+
+
+# In[19]:
+
+d=d+200
+
+
+# In[22]:
+
+print id(l1)
+print l1
+
+
+# In[23]:
+
+l1.append(10)
+print id(l1)
+print l1
+
+
+# In[24]:
+
+print l3
+
+
+# In[25]:
+
+# immutable objects
+a=100
+b=200
+c=a
+d=100
+print id(a),id(b),id(c),id(d)
+
+
+# In[28]:
+
+#mutable object
+l1=[10,20,30,40]
+l2=[50,60,70,80]
+l3=l1
+l4=[10,20,30,40]
+print id(l1),id(l2),id(l3),id(l4)
+
+
+# In[29]:
+
+
+# supports oops
+# scripting language (General purpose prgrming language )
+# can do webprograming, mobile apps, gaming, robotic, interact with devices, can write cloud app
+# can erp apps
+# easy to learn
+# opensource
+# dynamic typing language
+# automatic memory management
+# Scalable progrmaing language
+# protable progrming language
+# can run from the source code
+# extensible programing language
+
+
+# In[30]:
+
+a=10
+
+
+# In[31]:
+
+print type(a)
+
+
+# In[32]:
+
+a="python"
+
+
+# In[33]:
+
+# numpy 
+#math
+
+
+# In[ ]:
+
+
+
